150dd68b00e9eebf325272b845c6c5c1f3720890
pravshot <praveenkalva@icloud.com>
2023-09-18 18:42:39 -0500

    better student started code

--- src/color_detection.py
+++ src/color_detection.py
@@ -1,26 +1,34 @@
 # import necessary libraries(opencv, numpy)
-
-
+import cv2
+import numpy as np
 # read the color pallete image (assets/colors.jpg)
-
+img = None
 # convert image to HSV color space
-
+hsv_img = None
 
 # define the lower and upper bounds of the color we want to detect
-
+lower_range = None
+upper_range = None
 
 # create a mask for the color
-
+mask = None
 # show mask
-
+cv2.imshow('mask', mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # apply mask onto original image to only show the desired color
-
+result = None
 # show result
-
+cv2.imshow('result', result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # find contours from the mask
 
 # draw rectangles around each contour
 
 # show result
+cv2.imshow('contours', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- src/edge_detection.py
+++ src/edge_detection.py
@@ -1,16 +1,23 @@
 # import necessary libraries(opencv, numpy)
-
+import cv2
+import numpy as np
 
 # read the cat image (assets/cat1.jpg)
-
+img = None
 # convert the image to grayscale
-
+gray_img = None
 # Blur the image for better edge detection with (5,5) kernel
-
+blurred_img = None
 # Apply Canny edge detection with 100, 100 threshold
-
+edges = None
 
 # display the edges and wait for a keypress
-
+cv2.imshow('Edge detection', edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # BONUS: Try applying a dilation/erosion to the edges
+bonus_img = None
+cv2.imshow('Bonus', bonus_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
