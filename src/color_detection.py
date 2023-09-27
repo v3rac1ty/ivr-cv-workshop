04c3df37d14d9406aaa5eddf63e40861285e9481
Rishi V <rishiv5@illinois.edu>
2023-09-26 19:59:39 -0500

    cv workshop

--- src/color_detection.py
+++ src/color_detection.py
@@ -2,32 +2,35 @@
 import cv2
 import numpy as np
 # read the color pallete image (assets/colors.png)
-img = None
+img = cv2.imread("./assets/colors.png")
 # convert image to HSV color space
-hsv_img = None
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # define the lower and upper bounds of the color we want to detect
-lower_range = None
-upper_range = None
+lower_range = np.array([140, 50, 0])
+upper_range = np.array([170, 255, 255])
 
 # create a mask for the color
-mask = None
+mask = cv2.inRange(hsv_img, lower_range, upper_range)
+
 # show mask
 cv2.imshow('mask', mask)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # apply mask onto original image to only show the desired color
-result = None
+result = cv2.bitwise_and(img, img, mask=mask)
 # show result
 cv2.imshow('result', result)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # find contours from the mask
-
+countours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # draw rectangles around each contour
-
+for cnt in countours:
+    rect = cv2.boundingRect(cnt)
+    cv2.rectangle(img, rect, (0,0,0), 10)
 # show result
 cv2.imshow('contours', img)
 cv2.waitKey(0)
--- src/edge_detection.py
+++ src/edge_detection.py
@@ -3,13 +3,14 @@ import cv2
 import numpy as np
 
 # read the cat image (assets/cat1.jpg)
-img = None
+img = cv2.imread('./assets/cat1.jpg')
 # convert the image to grayscale
-gray_img = None
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Blur the image for better edge detection with (5,5) kernel
-blurred_img = None
+blurred_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
+
 # Apply Canny edge detection with 100, 100 threshold
-edges = None
+edges = cv2.Canny(blurred_img, 100, 100)
 
 # display the edges and wait for a keypress
 cv2.imshow('Edge detection', edges)
@@ -17,7 +18,8 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # BONUS: Try applying a dilation/erosion to the edges
-bonus_img = None
+bonus_img = cv2.dilate(edges, np.ones(10), iterations=4)
+# bonus_img = cv2.erode(edges, np.ones(3), iterations=1)
 cv2.imshow('Bonus', bonus_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
